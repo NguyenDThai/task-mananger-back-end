816addd4bcb9bec180ba900220c29aa8c565c2db
NguyenDThai <thainguyen4646@gmail.com>
2026-04-04 10:02:02 +0700

    thêm field urgent cho model task

--- src/models/Task.js
+++ src/models/Task.js
@@ -27,7 +27,7 @@ const taskSchema = new mongoose.Schema(
     },
     priority: {
       type: String,
-      enum: ['High', 'Medium', 'Low'],
+      enum: ['Urgent', 'High', 'Medium', 'Low'],
       default: 'Medium',
     },
     labels: [
